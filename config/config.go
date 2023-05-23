0a15b23f9425cb77e3c84b009e099f8818669082
Renan Dev <appsskilldeveloper@gmail.com>
2023-05-23 17:20:10 -0300

    fix

--- config/config.go
+++ config/config.go
@@ -15,8 +15,13 @@ func NewDB() *sql.DB {
 		log.Fatal("Erro ao carregar o arquivo .env:", err)
 	}
 	nameDB := os.Getenv("NAME_DB")
-	hostDB := os.Getenv("HOST_DB")
-	db, err := sql.Open("mysql", "root:1234@tcp("+hostDB+")/"+nameDB+"?parseTime=true")
+	portDB := os.Getenv("PORT_DB")
+	hostDB := os.Getenv("HOST_DB") // 3306
+
+	userDB := os.Getenv("USER_DB")      // root
+	passwordDB := os.Getenv("PASSW_DB") // 1234
+
+	db, err := sql.Open("mysql", userDB+":"+passwordDB+"@tcp("+hostDB+":"+portDB+")/"+nameDB+"?parseTime=true")
 	if err != nil {
 		panic(err)
 	}
