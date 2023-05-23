3d46038d7d5fbfdcd44da8ee88d7a1d22ae7be01
Renan Dev <appsskilldeveloper@gmail.com>
2023-05-23 17:31:00 -0300

    fix

--- config/config.go
+++ config/config.go
@@ -3,7 +3,6 @@ package config
 import (
 	"database/sql"
 	"log"
-	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
@@ -14,14 +13,18 @@ func NewDB() *sql.DB {
 	if err != nil {
 		log.Fatal("Erro ao carregar o arquivo .env:", err)
 	}
-	nameDB := os.Getenv("NAME_DB")
-	portDB := os.Getenv("PORT_DB")
-	hostDB := os.Getenv("HOST_DB") // 3306
+	/*
+		nameDB := os.Getenv("NAME_DB")
+		portDB := os.Getenv("PORT_DB")
+		hostDB := os.Getenv("HOST_DB") // 3306
 
-	userDB := os.Getenv("USER_DB")      // root
-	passwordDB := os.Getenv("PASSW_DB") // 1234
+		userDB := os.Getenv("USER_DB")      // root
+		passwordDB := os.Getenv("PASSW_DB") // 1234
+	*/
+	//db, err := sql.Open("mysql", userDB+":"+passwordDB+"@tcp("+hostDB+":"+portDB+")/"+nameDB+"?parseTime=true")
+	db, err := sql.Open("mysql", "root:L4pVdHeLelDqFxUGIRax@containers-us-west-4.railway.app:7574/railway?parseTime=true")
 
-	db, err := sql.Open("mysql", userDB+":"+passwordDB+"@tcp("+hostDB+":"+portDB+")/"+nameDB+"?parseTime=true")
+	//	db, err := sql.Open("mysql://root:L4pVdHeLelDqFxUGIRax@containers-us-west-4.railway.app:7574/railway?parseTime=true")
 	if err != nil {
 		panic(err)
 	}
